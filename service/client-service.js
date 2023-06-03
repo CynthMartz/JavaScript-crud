fdb1231fb38978fffe810c52c6552c0de72da8c5
CynthMartz <carol.martz21@gmail.com>
2023-06-02 21:24:05 -0400

    editar cliente

--- service/client-service.js
+++ service/client-service.js
@@ -7,7 +7,8 @@
 //Delete - Delete   
 
 //Fetch API
-const listaClientes = () => fetch("http://localhost:3000/perfil").then((respuesta) => respuesta.json());
+const listaClientes = () =>
+    fetch("http://localhost:3000/perfil").then((respuesta) => respuesta.json());
 
 const crearCliente = (nombre, email) => {
     return fetch("http://localhost:3000/perfil", {
@@ -25,12 +26,31 @@ const eliminarCliente = (id) => {
     });
 };
 
+const detalleCliente = (id) => {
+    return fetch(`http://localhost:3000/perfil/${id}`).then((respuesta) =>
+        respuesta.json()
+    );
+};
+
+const actualizarCliente = (nombre, email, id) => {
+    return fetch(`http://localhost:3000/perfil/${id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ nombre, email }),
+    })
+        .then((respuesta) => respuesta)
+        .catch((err) => console.log(err));
+};
+
 export const clientServices = {
     listaClientes,
     crearCliente,
     eliminarCliente,
+    detalleCliente,
+    actualizarCliente,
 };
-
 
 
 
